5cf84585c82023d400dc9e3aa34e7aaf5a25024e
Julienne <senanjulienne06@gmail.com>
2025-11-16 23:54:56 +0100

    Upadate de la table en demandant à l'utilisateur d'entrer la base

--- table.py
+++ table.py
@@ -1,10 +1,11 @@
+base=int(input("Veillez indiquer le nombre dont vous voulez la table de multiplication :"))
+debut=int(input("Par quel nombre doit commencer la table de multiplication ? :"))
+fin=int(input("Par quel nombre doit terminer la table de multiplication ? :"))
+inc=int(input("Vous voulez par bond de combien ? :"))
 def table (base,debut,fin,inc):
     i=debut
     for i in range(debut,fin,inc):
         print(base,"*",i,"=",base*i)
         i+=1
 
-table(2,1,11,1)
-table(5,1,11,1)
-table(3,1,11,1)
-table(4,1,11,1)
+table(base,debut,fin,inc)
